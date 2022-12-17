e595ab4f0eff044366509fdf704a79f5be27c577
Mehedi <mehedi@augnitive.com>
2022-12-17 19:04:35 +0600

    initial commit

---
+++ Chapter - 01 String Manipulation/string_manipulation_exercises/test/string_manipulation_exercises_test.dart
@@ -0,0 +1,8 @@
+import 'package:string_manipulation_exercises/string_manipulation_exercises.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
