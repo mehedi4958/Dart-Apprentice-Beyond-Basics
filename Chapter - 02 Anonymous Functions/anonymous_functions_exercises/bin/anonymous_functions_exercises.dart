af19b95547a97b97715dff60bd7f42f68642a336
Mehedi <mehedi@augnitive.com>
2022-12-29 11:04:48 +0600

    chapter 2 project created

---
+++ Chapter - 02 Anonymous Functions/anonymous_functions_exercises/bin/anonymous_functions_exercises.dart
@@ -0,0 +1,5 @@
+import 'package:anonymous_functions_exercises/anonymous_functions_exercises.dart' as anonymous_functions_exercises;
+
+void main(List<String> arguments) {
+  print('Hello world: ${anonymous_functions_exercises.calculate()}!');
+}
