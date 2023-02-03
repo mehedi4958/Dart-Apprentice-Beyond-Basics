a67dcd87d150da3cf3b0ba978a28e18ff3bf5e94
Mehedi <mehedi4958@gmail.com>
2023-02-03 21:15:05 +0600

    dart version upgraded

--- Chapter - 02 Anonymous Functions/anonymous_functions_exercises/test/anonymous_functions_exercises_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:anonymous_functions_exercises/anonymous_functions_exercises.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
