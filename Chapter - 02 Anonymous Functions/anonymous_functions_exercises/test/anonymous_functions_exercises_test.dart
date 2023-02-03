d74106d3a252d350a3c1d636384010d5eb8570bc
Mehedi <mehedi4958@gmail.com>
2023-02-03 22:16:01 +0600

    chapter2 created

---
+++ Chapter - 02 Anonymous Functions/anonymous_functions_exercises/test/anonymous_functions_exercises_test.dart
@@ -0,0 +1,8 @@
+// import 'package:anonymous_functions_exercises/anonymous_functions_exercises.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
