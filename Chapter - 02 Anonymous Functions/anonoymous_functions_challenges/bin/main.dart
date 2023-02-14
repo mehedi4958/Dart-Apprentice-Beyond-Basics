62eb524b2084bea554711f770fe70cd03786f40c
Mehedi <mehedi4958@gmail.com>
2023-02-14 21:03:24 +0600

    chapter 2 challenge 1 completed

---
+++ Chapter - 02 Anonymous Functions/anonoymous_functions_challenges/bin/main.dart
@@ -0,0 +1,9 @@
+import 'package:anonoymous_functions_challenges/challenge_01.dart';
+
+void main() {
+  print('Chapter - 02');
+
+  print('\tChallenge-01:');
+  final animals = AnimalsWithS();
+  animals.totalAnimalsWithS();
+}
