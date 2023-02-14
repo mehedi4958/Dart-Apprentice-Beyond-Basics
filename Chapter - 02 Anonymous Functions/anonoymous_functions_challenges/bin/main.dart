4abe982941419312771f748f1d566ea9d3caa643
Mehedi <mehedi4958@gmail.com>
2023-02-14 23:00:20 +0600

    folder deleted

--- Chapter - 02 Anonymous Functions/anonoymous_functions_challenges/bin/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:anonoymous_functions_challenges/challenge_01.dart';
-
-void main() {
-  print('Chapter - 02');
-
-  print('\tChallenge-01:');
-  final animals = AnimalsWithS();
-  animals.totalAnimalsWithS();
-}
